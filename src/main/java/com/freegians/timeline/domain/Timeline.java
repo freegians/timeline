2423130ae027b5c64873c7861cdd6394713a52f8
freegians <freegians@gmail.com>
2016-08-02 17:19:18 +0900

    #2 타임라인 테이블 스키마 작성

---
+++ src/main/java/com/freegians/timeline/domain/Timeline.java
@@ -0,0 +1,31 @@
+package com.freegians.timeline.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by freegians on 2016. 8. 1..
+ */
+
+@Entity (name = "timeline")
+@Data
+public class Timeline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private long id;
+    @Column(name = "USER_ID")
+    private long userId;
+    @Column(name = "WRITER_ID")
+    private long followerId;
+    @Column(name = "WRITER_NAME")
+    private String writerName;
+    @Column(name = "TIMELINE_TEXT")
+    private String timelineText;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CREATED_DATE")
+    private Date createdDate;
+
+}
